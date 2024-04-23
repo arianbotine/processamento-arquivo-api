fa5f48670af19c614ad4e64b773d4094e4648279
Arian Botine <arian.botine@shift.com.br>
2024-04-22 23:23:54 -0300

    Adicionando documentação do projeto

--- database-docker/criar-tabela.sql
+++ database-docker/criar-tabela.sql
@@ -1,26 +1,31 @@
 CREATE TABLE usuario (
-    id BIGINT PRIMARY KEY NOT NULL UNIQUE,
+    id BIGINT PRIMARY KEY NOT NULL,
     nome VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE pedido (
     id BIGINT PRIMARY KEY NOT NULL,
     data_compra DATE NOT NULL,
-    id_usuario BIGSERIAL NOT NULL,
+    id_usuario BIGINT NOT NULL,
     valor_total DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
 
 CREATE INDEX idx_data_compra ON pedido (data_compra);
 
-CREATE TABLE pedido_produto_valor (
+CREATE TABLE produto (
+    id BIGINT PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE pedido_produto (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     id_pedido BIGINT NOT NULL,
     id_produto BIGINT NOT NULL,
     valor DECIMAL(10,2) NOT NULL,
-    CONSTRAINT pedido_produto UNIQUE (id_pedido,id_produto),
-    FOREIGN KEY (id_pedido) REFERENCES pedido(id)
+    CONSTRAINT unique_pedido_produto UNIQUE (id_pedido, id_produto),
+    FOREIGN KEY (id_pedido) REFERENCES pedido(id),
+    FOREIGN KEY (id_produto) REFERENCES produto(id)
 );
 
-CREATE INDEX idx_id_pedido ON pedido_produto_valor (id_pedido);
-CREATE INDEX idx_id_produto ON pedido_produto_valor (id_produto);
+CREATE INDEX idx_id_pedido ON pedido_produto (id_pedido);
+CREATE INDEX idx_id_produto ON pedido_produto (id_produto);
